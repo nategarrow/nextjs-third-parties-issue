55c4628b445ef5024bb82dd6971eb1af83f1d7fe
Nathan G <ngarrow@webstacks.com>
2024-07-08 10:59:38 -0500

    add next/third-parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-XXXXXX" /> {/* TODO: Enter Valid ID */}
       <body className={inter.className}>{children}</body>
     </html>
   );
